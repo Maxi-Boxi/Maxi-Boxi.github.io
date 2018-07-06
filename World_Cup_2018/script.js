9efa74ae75a6f22afe010cf565ab063da4c2667c
Maxi-Boxi <39083071+Maxi-Boxi@users.noreply.github.com>
2018-07-07 02:09:02 +0600

    Add files via upload

--- World_Cup_2018/script.js
+++ World_Cup_2018/script.js
@@ -151,3 +151,28 @@ var tab1Get = document.getElementById('tab_1'),
 };
 
 
+/*----------Upper----------*/
+
+jQuery(function(){
+$("#Go_Top").removeAttr("href");
+if ($(window).scrollTop()>="250") $("#Go_Top").addClass("up")
+$(window).scroll(function(){
+if ($(window).scrollTop()<="250") $("#Go_Top").removeClass("up")
+else $("#Go_Top").addClass("up")
+});
+
+$("#Go_Bottom").removeAttr("href");
+if ($(window).scrollTop()<=$(document).height()-"999") $("#Go_Bottom").addClass("down")
+$(window).scroll(function(){
+if ($(window).scrollTop()>=$(document).height()-"999") $("#Go_Bottom").removeClass("down")
+else $("#Go_Bottom").addClass("down")
+});
+
+$("#Go_Top").click(function(){
+$("html, body").animate({scrollTop:0},"slow"); return false;
+})
+$("#Go_Bottom").click(function(){
+$("html, body").animate({scrollTop:$(document).height()},"slow"); return false;
+})
+});
+
